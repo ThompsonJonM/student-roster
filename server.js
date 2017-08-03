4543a7b98a1ca5a61c08251ed4ad07243ed859ff
Jonathan Thompson <thompson.jonm@gmail.com>
2017-08-02 21:29:41 -0500

    end of class

--- server.js
+++ server.js
@@ -1,13 +1,55 @@
 // Dependencies
 var express = require("express");
+var bodyParser = require("body-parser");
 var app = express();
 
 var PORT = 3000;
+
+// Allows for adding data via POST method
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+var roster = [
+    {
+        name: "Master Ting",
+        stance: "Tiger"
+    },
+    {
+        name: "Master Sha",
+        stance: "Ox"
+    },
+    {
+        name: "Master Qi",
+        stance: "Crane"
+    }
+];
 
 // Define GET route to return welcome message
 
 app.get("/", function(req, res) {
     res.send("Welcome to the Peak");
+});
+
+// GET route that returns three students
+
+app.get("/students", function(req, res) {
+    res.json(roster);
+});
+
+app.get("/students/:rosterId", function(req, res) {
+    res.json(roster[
+        req.params.rosterId
+    ]);
+});
+
+app.post("/add", function(req, res) {
+    console.log(req.body);
+    if (req.body.name) {
+        roster.push(req.body);
+        res.send("Student added");
+    }
 });
 
 app.listen(PORT, function() {
