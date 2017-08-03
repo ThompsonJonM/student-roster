714797f32c6e2541d7bf94ad66d069207d881f45
Jonathan Thompson <thompson.jonm@gmail.com>
2017-08-02 20:41:03 -0500

    GET request added

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var app = express();
 
 var PORT = 3000;
 
+// Define GET route to return welcome message
+
+app.get("/", function(req, res) {
+    res.send("Welcome to the Peak");
+});
+
 app.listen(PORT, function() {
     console.log(`Listening on PORT ${PORT}`)
 });
